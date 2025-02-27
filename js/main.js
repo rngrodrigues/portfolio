023f2555a11701983e843cf5cd342db19fa4ed76
Renan Rodrigues <renangithub2002@gmail.com>
2025-02-27 20:38:49 -0300

    Responsividade geral do site, e inicio da criação dos cards de habilidades

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 function alterarTema() {
     document.querySelector("body").classList.toggle("modo-escuro");
 }
+window.onscroll = function () {
+    let menu = document.getElementById("menu");
+    if (window.scrollY > 50) {
+        menu.classList.add("scrolled");
+    } else {
+        menu.classList.remove("scrolled");
+    }
+};
+
+// Garante que o menu já carregue corretamente caso a página abra rolada
+window.onscroll();
