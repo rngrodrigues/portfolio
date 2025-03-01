dd40134870d80589483698c7a4308107e02758c9
Renan Rodrigues <renangithub2002@gmail.com>
2025-03-01 19:53:32 -0300

    Implementação de cards responsivos, melhoria no modo escuro e alteração de algumas cores

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 function alterarTema() {
-    document.querySelector("body").classList.toggle("modo-escuro");
+    const body = document.body;
+    const menu = document.getElementById("menu");
+
+    body.classList.toggle("modo-escuro");
+    if (menu.classList.contains("scrolled") && body.classList.contains("modo-escuro")) {
+        menu.classList.add("scrolled-escuro");
+    } else {
+        menu.classList.remove("scrolled-escuro");
+    }
+    document.querySelectorAll(".card-hab").forEach(el => {
+        el.classList.toggle("card-hab-escuro");
+    });
 }
-window.onscroll = function () {
-    let menu = document.getElementById("menu");
-    if (window.scrollY > 50) {
+
+
+window.addEventListener("scroll", () => {
+    const menu = document.getElementById("menu");
+
+    if (window.scrollY > 70) {
         menu.classList.add("scrolled");
     } else {
         menu.classList.remove("scrolled");
     }
-};
+    if (menu.classList.contains("scrolled") && document.body.classList.contains("modo-escuro")) {
+        menu.classList.add("scrolled-escuro");
+    } else {
+        menu.classList.remove("scrolled-escuro");
+    }
+});
 
-// Garante que o menu já carregue corretamente caso a página abra rolada
-window.onscroll();
+window.dispatchEvent(new Event("scroll"));
