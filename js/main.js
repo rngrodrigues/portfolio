3d682a2e9508770bab072fef0efbfd25b4e7becb
Renan Rodrigues <renangithub2002@gmail.com>
2025-03-13 20:54:14 -0300

    Finalização da seção de contato, e alguns ajustes em cores e tamanhos de letra

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 function alterarTema() {
     const body = document.body;
     const menu = document.getElementById("menu");
-
+    const projetos = document.getElementById("projetos"); 
+    const sobre = document.getElementById("sobre");
     body.classList.toggle("modo-escuro");
+    
     document.querySelectorAll('.texto-grid').forEach(el => el.classList.toggle('texto-grid-noturno'));
     if (menu.classList.contains("scrolled") && body.classList.contains("modo-escuro")) {
         menu.classList.add("scrolled-escuro");
@@ -12,8 +14,19 @@ function alterarTema() {
     document.querySelectorAll(".card-hab").forEach(el => {
         el.classList.toggle("card-hab-escuro");
     });
-    document.querySelectorAll(".")
+    document.querySelectorAll(".time-line-content").forEach(el => {
+        el.classList.toggle("time-line-content-noturno");
+    });
+  
+    if (projetos) {
+        projetos.classList.toggle("projetos-noturno"); 
+    }
+    if (sobre) {
+        sobre.classList.toggle("sobre-noturno"); 
+    }
+
 }
+
 
 
 window.addEventListener("scroll", () => {
@@ -32,3 +45,12 @@ window.addEventListener("scroll", () => {
 });
 
 window.dispatchEvent(new Event("scroll"));
+
+const primeiraLista = document.querySelector(".lista");
+
+const segundaLista = primeiraLista.cloneNode(true);
+segundaLista.setAttribute("aria-hidden", "true"); 
+
+
+document.querySelector(".div-icons-lang .wrapper").appendChild(segundaLista);
+
